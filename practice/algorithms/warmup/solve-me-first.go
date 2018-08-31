ac4f3d6d128a2ed0b280a0933227234b344dfbe4
Rishabh Jain <mailrishabh1403@gmail.com>
2018-09-01 00:33:08 +0530

    add solve me first solution

---
+++ practice/algorithms/warmup/solve-me-first.go
@@ -0,0 +1,21 @@
+/*
+
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://www.hackerrank.com/challenges/solve-me-first/problem
+blog for this code :- https://rishabh1403.com/posts/coding/hackerrank/2018/08/hackerrank-solve-me-first-solution/
+video explanation for this code :- https://www.youtube.com/watch?v=yNzVtH2xsfQ
+
+*/
+
+package main
+import (
+    "fmt"
+)
+
+func main(){
+    var a,b,sum int
+    fmt.Scanf("%d\n%d",&a,&b)
+    //fmt.Scan(&a,&b)
+    sum = a+b
+    fmt.Println(sum)
+}

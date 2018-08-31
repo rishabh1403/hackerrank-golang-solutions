41e9bdf1c5ab93a8eb16790611e26436244dfd9a
Rishabh Jain <mailrishabh1403@gmail.com>
2018-09-01 00:33:57 +0530

    add simple array sum  solution

---
+++ practice/algorithms/warmup/simple-array-sum.go
@@ -0,0 +1,21 @@
+/*
+
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://www.hackerrank.com/challenges/simple-array-sum/problem
+blog for this code :-https://rishabh1403.com/posts/coding/hackerrank/2018/08/hackerrank-solution-of-simple-array-sum-in-golang/
+video explanation for this code :- https://www.youtube.com/watch?v=lUftBPb2gx0
+
+*/
+
+package main
+import "fmt"
+
+func main(){
+    var n, temp,sum int
+    fmt.Scan(&n)
+    for i:=0;i<n;i++{
+        fmt.Scan(&temp)
+        sum += temp
+    }
+    fmt.Println(sum)
+}

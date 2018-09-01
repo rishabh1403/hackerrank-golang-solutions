c671dd3d732200254f122394908bab9c274ade99
Rishabh Jain <mailrishabh1403@gmail.com>
2018-09-01 16:42:37 +0530

    add code for staircase

---
+++ practice/algorithms/warmup/staircase.go
@@ -0,0 +1,27 @@
+/*
+
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://www.hackerrank.com/challenges/staircase/problem
+blog for this code :-https://rishabh1403.com/posts/coding/hackerrank/2018/09/hackerrank-solution-of-staircase-in-golang/
+video explanation for this code :- https://www.youtube.com/watch?v=f9hHJSxUfX4
+
+*/
+
+package main
+
+import "fmt"
+
+func main(){
+    var n int
+    fmt.Scan(&n)
+
+    for i:=0;i<n;i++{
+	for j:=0;j<n-i-1;j++{
+	    fmt.Print(" ")
+	}
+	for j:=0;j<=i;j++{
+	    fmt.Print("#")
+	}
+	fmt.Println()
+    }
+}

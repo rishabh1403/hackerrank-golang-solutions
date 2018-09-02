1d572bf895f805a0ca23521992bfab70eba8b559
Rishabh Jain <mailrishabh1403@gmail.com>
2018-09-02 18:26:54 +0530

    Hacker rank code update on Sun Sep  2 18:26:54 IST 2018

---
+++ practice/algorithms/warmup/mini-max-sum.go
@@ -0,0 +1,25 @@
+/*
+
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://www.hackerrank.com/challenges/mini-max-sum/problem
+blog for this code :-https://rishabh1403.com/posts/coding/hackerrank/2018/09/hackerrank-solution-of-mini-max-sum-in-golang/
+video explanation for this code :- https://www.youtube.com/watch?v=-LlUwKzx-0k
+
+*/
+package main
+
+import (
+    "fmt"
+    "sort"
+)
+
+func main(){
+
+    a := make([]int, 5)
+
+    fmt.Scan(&a[0],&a[1],&a[2],&a[3],&a[4])
+    sort.Ints(a)
+    min := a[0]+a[1]+a[2]+a[3]
+    max := a[1]+a[2]+a[3]+a[4]
+    fmt.Println(min,max)
+}

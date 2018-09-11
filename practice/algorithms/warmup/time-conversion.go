863090d6de890e7bee24ca0c514432cf7d201bb7
Rishabh Jain <mailrishabh1403@gmail.com>
2018-09-11 21:41:31 +0530

    Hacker rank code update on Tue Sep 11 21:41:31 IST 2018

---
+++ practice/algorithms/warmup/time-conversion.go
@@ -0,0 +1,25 @@
+/*
+
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://www.hackerrank.com/challenges/time-conversion/problem
+blog for this code :-https://rishabh1403.com/posts/coding/hackerrank/2018/09/hackerrank-solution-of-time-conversion-in-golang/
+video explanation for this code :- https://www.youtube.com/watch?v=f9hHJSxUfX4
+
+*/
+package main
+
+import "fmt"
+
+func main(){
+    var h,m,s int
+    var suffix string
+    fmt.Scanf("%d:%d:%d%s",&h,&m,&s,&suffix)
+    if suffix == "AM" && h == 12 {
+        h = 0
+    }
+    if suffix == "PM" && h != 12 {
+        h += 12
+    }
+    fmt.Printf("%02d:%02d:%02d",h,m,s)
+}
+

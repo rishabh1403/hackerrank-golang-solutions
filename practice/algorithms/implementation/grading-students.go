38c4745e93ab8e9a3a16acd5527d92d68d084b02
Rishabh Jain <mailrishabh1403@gmail.com>
2018-09-16 18:44:22 +0530

    Hacker rank code update on Sun Sep 16 18:44:22 IST 2018

---
+++ practice/algorithms/implementation/grading-students.go
@@ -0,0 +1,32 @@
+/*
+
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://www.hackerrank.com/challenges/grading/problem
+blog for this code :-https://rishabh1403.com/posts/coding/hackerrank/2018/09/hackerrank-solution-of-staircase-in-golang/
+video explanation for this code :- https://www.youtube.com/watch?v=f9hHJSxUfX4
+
+*/
+package main
+
+import (
+    "fmt"
+)
+
+func main(){
+
+    var n,t int
+    fmt.Scan(&n)
+
+    for i:=0;i<n;i++{
+        fmt.Scan(&t)
+        if t<38{
+            fmt.Println(t)
+        }else {
+            if t%5 >2 {
+                fmt.Println(((t/5)+1)*5)
+            }else {
+                fmt.Println(t)
+            }
+        }
+    }
+}

2c71aa6b938ffa66e5e3fdd4caaf1a019d28c2d3
Rishabh Jain <mailrishabh1403@gmail.com>
2018-10-06 22:02:02 +0530

    Hacker rank code update on Sat Oct  6 22:02:02 IST 2018

---
+++ practice/algorithms/implementation/divisible-sum-pairs.go
@@ -0,0 +1,33 @@
+/*
+
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
+blog for this code :-https://rishabh1403.com/posts/coding/hackerrank/2018/09/hackerrank-solution-of-staircase-in-golang/
+video explanation for this code :- https://www.youtube.com/watch?v=f9hHJSxUfX4
+
+*/
+package main
+
+import "fmt"
+
+func main(){
+	var n,k int
+	fmt.Scan(&n,&k)
+
+	a := make([]int,n)
+
+	for i:=0;i<n;i++{
+		fmt.Scan(&a[i])
+	}
+
+	c := 0
+
+	for i:=0;i<n;i++{
+		for j:=i+1;j<n;j++{
+			if (a[i]+a[j]) % k == 0{
+				c++
+			}
+		}
+	}
+	fmt.Println(c)
+}

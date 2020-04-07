0345acfc1490c95bc9693b20a866478591eb43e5
Rishabh Jain <mailrishabh1403@gmail.com>
2020-04-07 08:32:09 +0530

    add new drafts

--- practice/algorithms/implementation/drawing-book.go
+++ practice/algorithms/implementation/drawing-book.go
@@ -1,3 +1,12 @@
+/*
+
+Author :- Rishabh Jain <contact@rishabh1403.com>
+Solution for :- https://www.hackerrank.com/challenges/drawing-book/problem
+blog for this code :-https://rishabh1403.com/posts/coding/hackerrank/2020/04/hackerrank-solution-of-drawing-book-in-golang
+Youtube video :- https://youtu.be/yTbd7vRDF4Y
+
+*/
+
 package main
 
 import "fmt"
